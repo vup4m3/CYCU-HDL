da446e2258ea05ab32e3b0416a11b63096cd92c9
vup4m3 <anakinchen861101@gmail.com>
2020-05-28 18:35:45 +0800

    good

--- Assignment_3-4/source_reg.v
+++ Assignment_3-4/source_reg.v
@@ -1,60 +1,56 @@
 module Source_Reg(
-  input w,
-  input r,
   input clk,
   input [23:0] wdata,
   input aluop_st,
-  input [2:0]addr,
-  output reg [2:0]next_addr
-  output cap_en
+  input cap_en,
+  output reg alu_en;
+  output [23:0] out_data
 );
+
 // [23]R, [22]C, [21:19]Addr, [18:16]Cmd, [15:8]a, [7:0]b
+reg [2:0] addr;
+reg [3:0] cnt;
+// reg hold;
 reg [23:0] reg_0, reg_1, reg_2, reg_3, reg_4, reg_5, reg_5, reg_6, reg_7;
 
-always@(posedge clk) begin
-  if (w = 1 && wdata[21:19] == 0)
-    reg_0 = wdata;
-  else if (w = 1 && wdata[21:19] == 1)
-    reg_1 = wdata;
-  else if (w = 1 && wdata[21:19] == 2)
-    reg_2 = wdata;
-  else if (w = 1 && wdata[21:19] == 3)
-    reg_3 = wdata;
-  else if (w = 1 && wdata[21:19] == 4)
-    reg_4 = wdata;
-  else if (w = 1 && wdata[21:19] == 5)
-    reg_5 = wdata;
-  else if (w = 1 && wdata[21:19] == 6)
-    reg_6 = wdata;
-  else if (w = 1 && wdata[21:19] == 7)
-    reg_7 = wdata;
+always @( posedge cap_en ) begin
+  addr<=wdata[21:19];
 end
 
-always@(posedge clk) begin
-  if (aluop_st == 1)
-    next_addr = 0;
-  else if (addr == 0)
-    next_addr = 1;
-  else if (addr == 1)
-    next_addr = 2;
-  else if (addr == 2)
-    next_addr = 3;
-  else if (addr == 3)
-    next_addr = 4;
-  else if (addr == 4)
-    next_addr = 5;
-  else if (addr == 5)
-    next_addr = 6;
-  else if (addr == 6) begin
-    next_addr = 7;
-    capture_en = 0;
+always @(posedge clk ) begin
+  if (cap_en)begin
+    if(addr==0 )reg_0<=wdata;
+    else if(addr==1 )reg_1<=wdata;
+    else if(addr==2 )reg_2<=wdata;
+    else if(addr==3 )reg_3<=wdata;
+    else if(addr==4 )reg_4<=wdata;
+    else if(addr==5 )reg_5<=wdata;
+    else if(addr==6 )reg_6<=wdata;
+    else if(addr==7 )reg_7<=wdata;
   end
-
 end
 
-always@(posedge clk) begin
-    
+always @( posedge clk ) begin
+  alu_en<=aluop_st;
+end
 
+always @( posedge clk ) begin
+  if(!alu_en) cnt<=0;
+  else if( cnt<8 )begin
+    if(cnt==0)out_data<=reg_0;
+    else if(cnt==1)out_data<=reg_1;
+    else if(cnt==2)out_data<=reg_2;
+    else if(cnt==3)out_data<=reg_3;
+    else if(cnt==4)out_data<=reg_4;
+    else if(cnt==5)out_data<=reg_5;
+    else if(cnt==6)out_data<=reg_6;
+    else if(cnt==7)out_data<=reg_7;
+
+    cnt<=cnt+1;
+    
+  end
+  else if( cnt==8 ) cnt<=cnt;
+  
 end
 
 endmodule
--- Assignment_3-4/spi_ctrl.v
+++ Assignment_3-4/spi_ctrl.v
@@ -3,9 +3,44 @@ module Spi_Ctrl(
   input spi_clk,
   input [23:0]spi_wdata,
   input aluop_st,
-  input rst
-    
+  input rst,
+  output [23:0] spi_out,
+  output reg alu_en,
+  output reg cap_en
 );
 
+reg [23:0] shift_reg;
+reg [4:0] cnt;
+
+
+always@(posedge spi_clk) begin
+  if (rst == 1)
+    alu_en <= 0;
+  else
+    alu_en <= aluop_st;
+end
+
+always@ ( negedge spi_clk ) begin
+  if ( ! spi_en && cnt < 24 ) begin
+    shift_reg[ cnt ] <= spi_wdata[cnt];
+    cnt <= cnt+1;
+  end
+  else if ( cnt == 24 )
+    cap_en<=1;
+end
+
+always@( negedge spi_en )begin
+  cnt<=0;
+  shift_reg<=0;
+  cap_en<=0;
+end
+
+always@( posedge cap_en)begin
+  spi_out<=shift_reg;
+end
+
+always @( posedge ) begin
+  alu_en<=aluop_st;
+end
 
 endmodule // Spi_Ctrl
